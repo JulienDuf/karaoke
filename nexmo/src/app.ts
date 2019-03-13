cff434d5d2da59407b86894b3b2379b778e31c94
JulienDuf <dufresne_julien@hotmail.ca>
2019-03-13 15:44:32 -0400

    Sing me a song

--- nexmo/src/app.ts
+++ nexmo/src/app.ts
@@ -5,6 +5,8 @@ import * as cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import { Index } from './route';
+import * as session from 'express-session';
+import { Song } from './route/song';
 
 export class Application {
     public app: express.Application;
@@ -25,13 +27,25 @@ export class Application {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(cookieParser());
+        this.app.use(session({
+            name: "sing-me-a-song",
+            secret: "mysecretissecret",
+            resave: false,
+            saveUninitialized: true,
+            cookie: {
+                httpOnly: true,
+                path: "/"
+            }
+        }));
         this.app.use(express.static(path.join(__dirname, '../public')));
         this.app.use(cors());
     }
 
     public routes() {
         const index: Index = new Index();
+        const song: Song = new Song();
         this.app.use("/", index.router);
+        this.app.use("/song", song.router);
 
         this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
             let err = new Error('Not Found');
